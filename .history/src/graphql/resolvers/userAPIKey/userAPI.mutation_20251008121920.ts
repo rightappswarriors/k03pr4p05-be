50dc5d7383b4b8c3dd1ffcc69b26daf845210b52
rai.emmanuel-rusiana <rai.rusianaemmanuel@gmail.com>
2025-10-08 23:21:26 +0800

    updated the .gitignore to make the .history obsolete

--- .history/src/graphql/resolvers/userAPIKey/userAPI.mutation_20251008121920.ts
+++
@@ -1,9 +0,0 @@
-import { extendType, arg, nonNull, objectType } from "nexus";
-import * as userAPIKey from "../../../services/user.service.js";
-import {
-  requireAuth,
-  requireRole,
-} from "../../../middleware/auth.middleware.js";
-
-
-export const ApiMutation = 
